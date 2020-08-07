0004d174fd023efe432d3b67a2b41bd92090fd38
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-08-07 23:05:13 +0200

    feat: add ApplicationTests

--- src/test/java/com/nnk/springboot/PoseidonApplicationTests.java
+++ src/test/java/com/nnk/springboot/PoseidonApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nnk.springboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nnk.springboot.controllers.BidListController;
+import com.nnk.springboot.controllers.UserController;
 
 @SpringBootTest
 class PoseidonApplicationTests {
+	
+	@Autowired
+	private UserController controller;
+
+	@Autowired
+	private BidListController bidListController;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+		assertThat(bidListController).isNotNull();
 	}
 
 }
