2c342fca7f276fd40a2eacadab43c1a9362a0c66
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-08-14 01:06:27 +0200

    refactor: improve BidListService & Repository

--- src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -3,9 +3,18 @@ package com.nnk.springboot.repositories;
 import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/**
+ * Bid list repository interface that extends JPA Repository.
+ *
+ * @Thierry Schreiner
+ */
 public interface BidListRepository extends JpaRepository<BidList, Integer> {
 
-	BidList findByBidListId(Integer id);
+	/**
+	 * 
+	 * @param id
+	 * @return a BidList
+	 */
+    BidList findByBidListId(Integer id);
 
 }
--- src/main/java/com/nnk/springboot/services/BidListService.java
+++ src/main/java/com/nnk/springboot/services/BidListService.java
@@ -2,20 +2,46 @@ package com.nnk.springboot.services;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import com.nnk.springboot.domain.BidListDTO;
 
+/**
+ * This BidListService interface defines five methods in charge of driving the
+ * BidList CRUD operations using DatTransferObject.
+ *
+ * @author Thierry Schreiner
+ */
 public interface BidListService {
 
-	List<BidListDTO> findAll();
+    /**
+     * Used to get a list of all Poseidon bidLists stored in the bid_list table
+     * of the Database.
+     *
+     * @return a List<BidListDTO>
+     */
+    List<BidListDTO> findAll();
 
-	void add(@Valid BidListDTO bidListDTO);
+    /**
+     * Used to persist a Poseidon BidList in DataBase.
+     *
+     * @param bidListDTO
+     * @return a List<BidListDTO>
+     */
+    BidListDTO save(BidListDTO bidListDTO);
 
-	void update(@Valid BidListDTO bidListDTO);
+    /**
+     * Allows user to delete a Poseidon BidList of the DataBase.
+     *
+     * @param id
+     * @return a BidListDTO
+     */
+    BidListDTO delete(Integer id);
 
-	void delete(Integer id);
-
-	BidListDTO getById(Integer id);
+    /**
+     * Use to get the Poseidon bidList identified by the given id.
+     *
+     * @param id
+     * @return a BidListDTO
+     */
+    BidListDTO getById(Integer id);
 
 }
