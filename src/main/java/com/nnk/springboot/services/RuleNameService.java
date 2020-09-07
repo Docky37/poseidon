5d2833566bcffd3d8c6a979891af6145d48de233
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-09-07 11:15:29 +0200

    feat: add RatingServiceImpl code to make the tests pass

---
+++ src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -0,0 +1,50 @@
+package com.nnk.springboot.services;
+
+import java.util.List;
+
+import com.nnk.springboot.dto.RuleNameDTO;
+import com.nnk.springboot.exceptions.RuleNameNotFoundException;
+
+/**
+ * This RuleNameService interface defines four methods in charge of driving
+ * the RuleName CRUD operations using DataTransferObject.
+ *
+ * @author Thierry Schreiner
+ */
+public interface RuleNameService {
+
+    /**
+     * Used to get a list of all Poseidon RuleName stored in the rule_name
+     * table of the Database.
+     *
+     * @return a List<RuleName>
+     */
+    List<RuleNameDTO> findAll();
+
+    /**
+     * Used to persist a Poseidon RuleName in DataBase.
+     *
+     * @param ruleNameDTO
+     * @return a RuleNameDTO
+     */
+    RuleNameDTO save(RuleNameDTO ruleNameDTO);
+
+    /**
+     * Allows user to delete a Poseidon RuleName of the DataBase.
+     *
+     * @param id
+     * @return a RuleNameDTO
+     * @throws RuleNameNotFoundException
+     */
+    RuleNameDTO getById(int id) throws RuleNameNotFoundException;
+
+    /**
+     * Use to get the Poseidon RuleName identified by the given id.
+     *
+     * @param id
+     * @return a RuleNameDTO
+     * @throws RuleNameNotFoundException
+     */
+    RuleNameDTO delete(int id) throws RuleNameNotFoundException;
+
+}
---
+++ src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
@@ -0,0 +1,92 @@
+package com.nnk.springboot.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.domain.mapping.RuleNameMapping;
+import com.nnk.springboot.dto.RuleNameDTO;
+import com.nnk.springboot.exceptions.RuleNameNotFoundException;
+import com.nnk.springboot.repositories.RuleNameRepository;
+
+/**
+ * Implementation class of the RuleNameService interface, this class answer to
+ * RuleNameController request using RuleNameRepository and RuleNameMapping
+ * classes.
+ *
+ * @author Thierry Schreiner
+ */
+@Service
+public class RuleNameServiceImpl implements RuleNameService {
+
+    /**
+     * RuleNameRepository bean injected by Spring when service is created.
+     */
+    @Autowired
+    private RuleNameRepository ruleNameRepository;
+
+    /**
+     * RuleNameMapping bean injected by Spring when service is created.
+     */
+    @Autowired
+    private RuleNameMapping ruleNameMapping;
+
+    /**
+     * {@inheritDoc}
+     */
+     @Override
+    public List<RuleNameDTO> findAll() {
+         List<RuleName> listRuleName = new ArrayList<>();
+         listRuleName = ruleNameRepository.findAll();
+
+         List<RuleNameDTO> listRuleNameDTO = ruleNameMapping
+                 .mapAListOfRuleName(listRuleName);
+
+         return listRuleNameDTO;
+      }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RuleNameDTO save(RuleNameDTO ruleNameDTO) {
+        RuleName ruleName = ruleNameMapping.mapDTOToEntity(ruleNameDTO);
+        System.out.println(ruleName.toString());
+        RuleName savedRuleName = ruleNameRepository.save(ruleName);
+        System.out.println(savedRuleName.toString());
+        RuleNameDTO savedBidListDTO = ruleNameMapping.mapEntityToDTO(savedRuleName);
+        System.out.println(savedBidListDTO.toString());
+        return savedBidListDTO;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws RuleNameNotFoundException 
+     */
+    @Override
+    public RuleNameDTO getById(int id) throws RuleNameNotFoundException {
+        RuleName ruleName = ruleNameRepository.findById(id)
+                .orElseThrow(() -> new RuleNameNotFoundException(
+                        "No RuleName record exist for given id"));
+
+        return ruleNameMapping.mapEntityToDTO(ruleName);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws RuleNameNotFoundException 
+     */
+    @Override
+    public RuleNameDTO delete(int id) throws RuleNameNotFoundException {
+        RuleName ruleName = ruleNameRepository.findById(id)
+                .orElseThrow(() -> new RuleNameNotFoundException(
+                        "No RuleName record exist for given id"));
+        ruleNameRepository.deleteById(id);
+
+        return ruleNameMapping.mapEntityToDTO(ruleName);
+    }
+
+}
