46411da1e4c0ed0590497a6ef8d680491d137a75
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-09-09 08:53:02 +0200

    feat: add TradeMappingTest (TDD) and TradeMapping code

--- src/main/java/com/nnk/springboot/domain/mapping/TradeMapping.java
+++ src/main/java/com/nnk/springboot/domain/mapping/TradeMapping.java
@@ -1,38 +1,151 @@
 package com.nnk.springboot.domain.mapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.dto.TradeDTO;
 import com.nnk.springboot.dto.TradeFullDTO;
+import com.nnk.springboot.util.UserRetrieve;
 
+/**
+ * This class is used to perform bidirectional mapping between a Trade entity
+ * and a TradeDTO.
+ *
+ * @author Thierry Schreiner
+ */
+@Component
 public class TradeMapping {
 
-    public List<TradeDTO> mapAListOfTrade(List<Trade> listOfTrade) {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * Spring autowired UserRetrieve instance declaration.
+     */
+    @Autowired
+    private UserRetrieve userRetrieve;
+
+    /**
+     * This method is in charge of the mapping of a list of Trade entities to a
+     * list of TradeDTO. Use the mapEntityToDTO(Trade trade)as a sub method.
+     *
+     * @param listOfTrade
+     * @return a List<TradeDTO>
+     */
+    public List<TradeDTO> mapAListOfTrade(final List<Trade> listOfTrade) {
+        List<TradeDTO> listTradeDTO = new ArrayList<>();
+        for (Trade trade : listOfTrade) {
+            TradeDTO tradeDTO = mapEntityToDTO(trade);
+            listTradeDTO.add(tradeDTO);
+        }
+        return listTradeDTO;
     }
 
-    public Trade mapDTOToEntity(TradeDTO tradeDTO) {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * This method is in charge of the mapping of a TradeDTO to a
+     * Trade Entity.
+     *
+     * @param tradeDTO
+     * @return a Trade object
+     */
+    public Trade mapDTOToEntity(final TradeDTO tradeDTO) {
+        String connectedUser = userRetrieve.getLoggedUser();
+        Trade trade = new Trade();
+        trade.setTradeId(tradeDTO.getTradeId());
+        trade.setAccount(tradeDTO.getAccount());
+        trade.setType(tradeDTO.getType());
+        trade.setBuyQuantity(tradeDTO.getBuyQuantity());
+        trade.setCreationName(connectedUser);
+        trade.setCreationDate(LocalDateTime.now());
+        trade.setRevisionName(connectedUser);
+        trade.setRevisionDate(LocalDateTime.now());
+        return trade;
     }
 
-    public TradeDTO mapEntityToDTO(Trade trade) {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * This method is in charge of the mapping of a Trade entity to a TradeDTO.
+     *
+     * @param trade
+     * @return a TradeDTO object
+     */
+    public TradeDTO mapEntityToDTO(final Trade trade) {
+        TradeDTO tradeDTO = new TradeDTO();
+        tradeDTO.setTradeId(trade.getTradeId());
+        tradeDTO.setAccount(trade.getAccount());
+        tradeDTO.setType(trade.getType());
+        tradeDTO.setBuyQuantity(trade.getBuyQuantity());
+        return tradeDTO;
     }
 
-    public TradeFullDTO mapEntityToFullDTO(Trade trade) {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * This method is in charge of the mapping of a Trade entity to a
+     * TradeFullDTO.
+     *
+     * @param trade
+     * @return a TradeDTO
+     */
+    public TradeFullDTO mapEntityToFullDTO(final Trade trade) {
+        TradeFullDTO tradeDTO = new TradeFullDTO();
+        tradeDTO.setTradeId(trade.getTradeId());
+        tradeDTO.setAccount(trade.getAccount());
+        tradeDTO.setType(trade.getType());
+        tradeDTO.setBuyQuantity(trade.getBuyQuantity());
+        tradeDTO.setSellQuantity(trade.getSellQuantity());
+        tradeDTO.setBuyPrice(trade.getBuyPrice());
+        tradeDTO.setSellPrice(trade.getSellPrice());
+        tradeDTO.setTradeDate(trade.getTradeDate());
+        tradeDTO.setSecurity(trade.getSecurity());
+        tradeDTO.setTrader(trade.getTrader());
+        tradeDTO.setStatus(trade.getStatus());
+        tradeDTO.setBenchmark(trade.getBenchmark());
+        tradeDTO.setBook(trade.getBook());
+        tradeDTO.setCreationName(trade.getCreationName());
+        tradeDTO.setCreationDate(trade.getCreationDate());
+        tradeDTO.setRevisionName(trade.getRevisionName());
+        tradeDTO.setRevisionDate(trade.getRevisionDate());
+        tradeDTO.setDealName(trade.getDealName());
+        tradeDTO.setDealType(trade.getDealType());
+        tradeDTO.setSourceListId(trade.getSourceListId());
+        tradeDTO.setSide(trade.getSide());
+
+        return tradeDTO;
     }
 
+    /**
+     * This method is in charge of the mapping of a TradeFullDTO to a Trade
+     * entity.
+     *
+     * @param tradeDTO
+     * @return a Trade
+     */
     public Trade mapFullDTOToEntity(final TradeFullDTO tradeDTO) {
-        // TODO Auto-generated method stub
-        return null;
+        String connectedUser = userRetrieve.getLoggedUser();
+        Trade trade = new Trade();
+        trade.setTradeId(tradeDTO.getTradeId());
+        trade.setAccount(tradeDTO.getAccount());
+        trade.setType(tradeDTO.getType());
+        trade.setBuyQuantity(tradeDTO.getBuyQuantity());
+        trade.setSellQuantity(tradeDTO.getSellQuantity());
+        trade.setBuyPrice(tradeDTO.getBuyPrice());
+        trade.setSellPrice(tradeDTO.getSellPrice());
+        trade.setTradeDate(tradeDTO.getTradeDate());
+        trade.setSecurity(tradeDTO.getSecurity());
+        trade.setTrader(tradeDTO.getTrader());
+        trade.setStatus(tradeDTO.getStatus());
+        trade.setBenchmark(tradeDTO.getBenchmark());
+        trade.setBook(trade.getBook());
+        trade.setCreationName(tradeDTO.getCreationName());
+        trade.setCreationDate(tradeDTO.getCreationDate());
+        trade.setRevisionName(connectedUser);
+        trade.setRevisionDate(LocalDateTime.now());
+        trade.setDealName(tradeDTO.getDealName());
+        trade.setDealType(tradeDTO.getDealType());
+        trade.setSourceListId(tradeDTO.getSourceListId());
+        trade.setSide(tradeDTO.getSide());
+
+        return trade;
     }
 
 }
---
+++ src/test/java/com/nnk/springboot/model/mapping/TradeMappingTest.java
@@ -0,0 +1,119 @@
+package com.nnk.springboot.model.mapping;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.domain.mapping.TradeMapping;
+import com.nnk.springboot.dto.TradeDTO;
+import com.nnk.springboot.dto.TradeFullDTO;
+import com.nnk.springboot.util.UserRetrieve;
+
+@SpringJUnitConfig(value = TradeMapping.class)
+public class TradeMappingTest {
+
+    @Autowired
+    TradeMapping tradeMapping;
+
+    @MockBean
+    public UserRetrieve userRetrieve;
+
+    static List<Trade> listOfTrade = new ArrayList<>();
+    static List<TradeDTO> listOfTradeDTO = new ArrayList<>();
+    static TradeFullDTO tradeDTO = new TradeFullDTO();
+    static {
+        listOfTrade.add(new Trade());
+        listOfTrade.get(0).setTradeId(1);
+        listOfTrade.get(0).setAccount("Account1");
+        listOfTrade.get(0).setType("Type1");
+        listOfTrade.get(0).setBuyQuantity(new BigDecimal("20"));
+        listOfTrade.get(0).setCreationName("Testeur");
+        listOfTrade.get(0).setRevisionName("Testeur");
+        listOfTrade.add(new Trade());
+        listOfTrade.get(1).setTradeId(2);
+        listOfTrade.get(1).setAccount("Account2");
+        listOfTrade.get(1).setType("Type2");
+        listOfTrade.get(1).setBuyQuantity(new BigDecimal("20"));
+        listOfTrade.get(1).setCreationName("Testeur");
+        listOfTrade.get(1).setRevisionName("Testeur");
+
+        listOfTradeDTO.add(new TradeDTO());
+        listOfTradeDTO.get(0).setTradeId(1);
+        listOfTradeDTO.get(0).setAccount("Account1");
+        listOfTradeDTO.get(0).setType("Type1");
+        listOfTradeDTO.get(0).setBuyQuantity(new BigDecimal("20"));
+        listOfTradeDTO.add(new TradeDTO());
+        listOfTradeDTO.get(1).setTradeId(2);
+        listOfTradeDTO.get(1).setAccount("Account2");
+        listOfTradeDTO.get(1).setType("Type2");
+        listOfTradeDTO.get(1).setBuyQuantity(new BigDecimal("20"));
+
+        tradeDTO.setTradeId(2);
+        tradeDTO.setAccount("Account2");
+        tradeDTO.setType("Type2");
+        tradeDTO.setBuyQuantity(new BigDecimal("20"));
+        tradeDTO.setCreationName("Testeur");
+        tradeDTO.setRevisionName("Testeur");
+    }
+
+    @Test
+    public void givenAListOfTrade_whenMapToDTO_thenReturnsAListOfTradeDTO() {
+        // GIVEN
+        // WHEN
+        List<TradeDTO> resultList = tradeMapping
+                .mapAListOfTrade(listOfTrade);
+        // THEN
+        assertThat(resultList.toString())
+                .isEqualTo(listOfTradeDTO.toString());
+
+    }
+
+    @Test
+    public void givenATrade_whenMapToFullDTO_thenReturnsTradeFullDTO() {
+        // GIVEN
+        // WHEN
+        TradeFullDTO result = tradeMapping
+                .mapEntityToFullDTO(listOfTrade.get(1));
+        // THEN
+        assertThat(result.toString()).isEqualTo(tradeDTO.toString());
+
+    }
+
+    @Test
+    public void givenATradeDTO_whenMapToEntity_thenReturnsTrade() {
+        // GIVEN
+        given(userRetrieve.getLoggedUser()).willReturn("Testeur");
+        // WHEN
+        Trade result = tradeMapping.mapDTOToEntity(listOfTradeDTO.get(0));
+        // THEN
+        result.setCreationDate(null);
+        result.setRevisionDate(null);
+        assertThat(result.toString())
+                .isEqualTo(listOfTrade.get(0).toString());
+
+    }
+
+    @Test
+    public void givenATradeFullDTO_whenMapToEntity_thenReturnsTrade() {
+        // GIVEN
+        given(userRetrieve.getLoggedUser()).willReturn("Testeur");
+        // WHEN
+        Trade result = tradeMapping.mapFullDTOToEntity(tradeDTO);
+        // THEN
+        result.setCreationDate(null);
+        result.setRevisionDate(null);
+        assertThat(result.toString())
+                .isEqualTo(listOfTrade.get(1).toString());
+
+    }
+
+}
