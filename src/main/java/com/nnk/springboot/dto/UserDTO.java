5248ab6f9b68aeada1c491801c67e2abcff21b72
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-09-16 15:11:08 +0200

    feature: add password @Pattern in UserDTO

--- src/main/java/com/nnk/springboot/dto/UserDTO.java
+++ src/main/java/com/nnk/springboot/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
+import com.nnk.springboot.constants.Constants;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,18 +26,27 @@ public class UserDTO {
      * Username (mandatory to log in before using Poseidon).
      */
     @NotBlank(message = "Username is mandatory!")
+    @Size(min = Constants.USERNAME_MIN_SIZE, max = Constants.USERNAME_MAX_SIZE,
+            message = "Size of username: 7 min to 15 max")
     private String username;
 
     /**
      * User's password (mandatory to log in before using Poseidon).
      */
     @NotBlank(message = "Password is mandatory!")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])"
+            + "[a-zA-Z0-9!@#$%^&*](?=\\S+$).{8,15}$" , 
+            message=" 8 to 15 characters, at least 1 number, 1 upper case &"
+                    + " 1 lower case letter, 1 special character !@#$%^&*"
+                    + " and no space")
     private String password;
 
     /**
      * User's full name.
      */
     @NotBlank(message = "FullName is mandatory")
+    @Size(min = Constants.FULLNAME_MIN_SIZE, max = Constants.FULLNAME_MAX_SIZE,
+            message = "Size of username: 7 min to 15 max")
     private String fullname;
 
     /**
